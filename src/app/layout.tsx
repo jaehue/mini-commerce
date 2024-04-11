63778ccf5f8d7cb818e6ef13a517e7e1b950996b
jaehue <bbugguj@gmail.com>
2024-04-11 15:16:00 +0900

    add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
 import "./globals.css";
 import Link from "next/link";
+import { Home, ShoppingCart } from "lucide-react";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Mini Commerce",
@@ -17,10 +23,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <header className="flex justify-between px-10 py-4">
-          <Link href="/">mini commerce</Link>
-          <Link href="/cart">cart</Link>
+          <Link href="/" className="flex gap-2 items-center">
+            <Home />
+            <span className="text-xl">mini commerce</span>
+          </Link>
+          <Link href="/cart">
+            <ShoppingCart />
+          </Link>
         </header>
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
           {children}
